f539e6212296f9453c372828a35bbe958c7a8130
axelbraunschweiger <axelbraunschweiger@gmail.com>
2019-01-22 11:35:11 -0500

    Language config

---
+++ typo3conf/ext/videotraining/Configuration/TypoScript/Modules/0000_config.ts
@@ -0,0 +1,42 @@
+config {
+
+	sys_language_uid = 0
+	language = de
+	locale_all = de_DE.utf-8
+	htmlTag_langKey = de
+
+
+}
+
+
+##English
+[globalVar = GP:L=1]
+
+	config {
+
+		sys_language_uid = 1
+		language = en
+		locale_all = en_GB.utf_8
+		htmlTag_langKey = en
+		sys_language_overlay = hideNonTranslated
+
+
+		linkVars = L
+	}
+
+[global]
+
+
+##French
+[globalVar = GP:L=2]
+
+	config {
+
+		sys_language_uid = 2
+		language = fr
+		locale_all = fr_FR.utf-8
+		htmlTag_langKey = fr
+	}
+
+[global]
+
